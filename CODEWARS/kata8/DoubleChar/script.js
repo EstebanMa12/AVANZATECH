6fead6e48f11731ed1c62ccf8d96acd31b4eed08
EstebanMa12 <desteban.maya@udea.edu.co>
2023-06-30 13:14:50 -0500

    Más codewars

---
+++ CODEWARS/kata8/DoubleChar/script.js
@@ -0,0 +1,3 @@
+function doubleChar(str) {
+    return str.split('').map(element=>element+element).join('')
+  }
---
+++ CODEWARS/kata8/countSheep/script.js
@@ -0,0 +1,7 @@
+var countSheep = function (num){
+    let a= ''
+    for (let i =1; i<=num;i++){
+      a+=(`${i} sheep...`)
+    }
+    return a
+  }

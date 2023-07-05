87bc7f4753a6241b86183f5641c52ca4424b2fbe
EstebanMa12 <desteban.maya@udea.edu.co>
2023-07-05 12:54:29 -0500

    Ejercicio de Avanzatech

--- EJERCICIOS/ejerciciosPropuestos/elDeAvanzatech/script.js
+++ EJERCICIOS/ejerciciosPropuestos/elDeAvanzatech/script.js
@@ -4,3 +4,29 @@ const array = ["Avanzatech","Xvxxxxxxxx","X.a.xxxxxx","X..n.xxxxx","Xvxxz.xxxx",
 const sinXArray = array.map(element => element.replace(/x/gi, ''));
 
 console.log(sinXArray);
+
+
+
+function diagonal(array){
+    let diagonal=''
+    for(let i =0; i<array.length;i++){
+        const arreglo=[]
+        for (let j  = 0; j <= i; j++) {
+            arreglo.push(array[j][j+i]);        
+        }
+    diagonal += arreglo[0];
+    }
+    return diagonal
+}
+
+
+function numberAvanzatech(array) {
+    const busqueda = "avanzatech"
+    let count = array.filter(arr =>arr.toUpperCase().includes(busqueda.toUpperCase())).length
+    if(busqueda.toUpperCase()===diagonal(array).toUpperCase()){
+        count+=1;
+    }
+    return count
+}
+
+  console.log(numberAvanzatech(array));
